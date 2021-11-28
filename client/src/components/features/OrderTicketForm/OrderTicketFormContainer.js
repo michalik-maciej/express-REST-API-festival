2a87672dd00e392950c0ff79a51f227be1b94c89
michalik-maciej <halniak24@gmail.com>
2021-11-28 20:41:04 +0100

    Init client and add loadSeats refresh

---
+++ client/src/components/features/OrderTicketForm/OrderTicketFormContainer.js
@@ -0,0 +1,15 @@
+import { addSeatRequest, getRequests, loadSeatsRequest } from '../../../redux/seatsRedux';
+
+import OrderTicketForm from './OrderTicketForm';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => ({
+  requests: getRequests(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  addSeat: (seat) => dispatch(addSeatRequest(seat)),
+  loadSeats: () => dispatch(loadSeatsRequest()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(OrderTicketForm);
